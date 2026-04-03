28c5d002643a61cbcbe72aa6ba47a1f4408a02c9
CesareFortunato <cesare.fortunato89@gmail.com>
2026-04-03 16:59:13 +0200

    feat: create new functions.php + set include in index

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php
-function setNewPass($lunghezza_pass)
-{
-
-    $uppercase = range('A', 'Z');
-    $lowercase = range('a', 'z');
-    $digits = range('0', '9');
-    $allChars = array_merge($uppercase, $lowercase, $digits);
-    $allChars = array_unique($allChars);
-
-    $randomKey = array_rand($allChars);
-
-
-    $newPass = "";
-    for ($i = 0; $i < $lunghezza_pass; $i++) {
-        $randomKey = array_rand($allChars);
-        $newPass .= $allChars[$randomKey];
-    }
-
-
-    return $newPass;
-
-
-}
+include 'functions.php';
 ?>
 
 <!DOCTYPE html>
